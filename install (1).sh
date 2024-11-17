c50f54e460e15a819772a79eadeca6a1b90d6b8b
Wilson Corn <wilson.corn@hotmail.com>
2024-11-17 10:29:08 -0800

    Update install (1).sh

--- install (1).sh
+++ install (1).sh
@@ -5,52 +5,7 @@ main() {
     echo -e "Welcome to the MacSploit Experience!"
     echo -e "Install Script Version 2.6"
 
-    echo -ne "Checking License..."
-    curl -s "https://git.raptor.fun/main/jq-macos-amd64" -o "./jq"
-    chmod +x ./jq
-    
-    curl -s "https://git.raptor.fun/sellix/hwid" -o "./hwid"
-    chmod +x ./hwid
-    
-    local user_hwid=$(./hwid)
-    local hwid_info=$(curl -s "https://git.raptor.fun/api/whitelist?hwid=$user_hwid")
-    local hwid_resp=$(echo $hwid_info | ./jq -r ".success")
-    rm ./hwid
-    
-    if [ "$hwid_resp" != "true" ]
-    then
-        echo -ne "\rEnter License Key:       \b\b\b\b\b\b"
-        read input_key
-
-        echo -n "Contacting Secure Api... "
-        
-        local resp=$(curl -s "https://git.raptor.fun/api/sellix?key=$input_key&hwid=$user_hwid")
-        echo -e "Done.\n$resp"
-        
-        if [ "$resp" != 'Key Activation Complete!' ]
-        then
-            rm ./jq
-            exit
-            return
-        fi
-    else
-        local free_trial=$(echo $hwid_info | ./jq -r ".free_trial")
-        if [ "$free_trial" == "true" ]
-        then
-            echo -ne "\rEnter License Key (Press Enter to Continue as Free Trial): "
-            read input_key
-            
-            if [ "$input_key" != 'gg' ]
-            then
-                echo -n "Contacting Secure Api... "
-                
-                local resp=$(curl -s "https://git.raptor.fun/api/sellix?key=$input_key&hwid=$user_hwid")
-                echo -e "Done.\n$resp"
-            fi
-        else
-            echo -e " Done.\nWhitelist Status Verified."
-        fi
-    fi
+    echo -e "No license required! Enjoy MacSploit for free!"
 
     echo -e "Downloading Latest Roblox..."
     [ -f ./RobloxPlayer.zip ] && rm ./RobloxPlayer.zip
@@ -68,7 +23,6 @@ main() {
         curl "http://setup.rbxcdn.com/mac/$version-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
     fi
     
-    rm ./jq
     echo -n "Installing Latest Roblox... "
     [ -d "/Applications/Roblox.app" ] && rm -rf "/Applications/Roblox.app"
     unzip -o -q "./RobloxPlayer.zip"
